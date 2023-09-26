d5444adc0c4300f6c374fa9469d4eee533c1c23a
Mohamed-Gamal-2 <141522329+Mohamed-Gamal-2@users.noreply.github.com>
2023-09-26 18:23:55 +0300

    Finishing Movie-Card Comp with all functionalities except for those which need users such as rating

--- src/app/movies/movie-card/movie-card.component.ts
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.css'],
 })
 export class MovieCardComponent {
+  imgPrefex = 'https://image.tmdb.org/t/p/w500/';
+  @Input() movie: any;
+  progress: any;
+  constructor(private _router: Router) {}
 
+  ngOnInit() {
+    this.progress = this.movie.vote_average * 10;
+  }
+
+  routingToDetails(id: number) {
+    this._router.navigate(['movie', id]);
+  }
 }

c750fafb16f7a11183b380a6f29b65b2853cc056
Mohamed-Gamal-2 <141522329+Mohamed-Gamal-2@users.noreply.github.com>
2023-09-26 11:44:41 +0300

    Add function to get all movies in movie-services and tested it

--- src/app/movies/all-movies/all-movies.component.ts
+++ src/app/movies/all-movies/all-movies.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MoviesApiService } from '../services/movies-api.service';
 
 @Component({
   selector: 'app-all-movies',
   templateUrl: './all-movies.component.html',
-  styleUrls: ['./all-movies.component.css']
+  styleUrls: ['./all-movies.component.css'],
 })
 export class AllMoviesComponent {
+  constructor(private _movies: MoviesApiService) {}
 
+  ngOnInit() {
+    this._movies.getMoviesList().subscribe((data) => console.log(data));
+  }
 }
--- src/app/movies/services/movies-api.service.ts
+++ src/app/movies/services/movies-api.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesApiService {
-
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+  getMoviesList() {
+    return this._http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=b6bf914c5259361379673d87ba12221b'
+    );
+  }
 }

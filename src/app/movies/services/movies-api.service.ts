af8cd2f22e05b5e2ef06d2ed9e5b7924161b24bd
Ellithy <engmohamedellithy13@gmail.com>
2023-09-30 13:02:09 +0300

    Post movie to wishlist Api and get it

--- src/app/movies/services/movies-api.service.ts
+++ src/app/movies/services/movies-api.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesApiService {
   private movie;
+  foundedMovie!: any;
   constructor(private _http: HttpClient) {
     this.movie = new BehaviorSubject({});
   }
@@ -32,5 +33,49 @@ export class MoviesApiService {
   }
   setMovie(movie: any) {
     this.movie.next(movie);
+  }
+  addToWatchlist(movieId: number) {
+    const url = 'https://api.themoviedb.org/3/account/20505794/watchlist'; // Replace with the appropriate URL
+    const body = {
+      media_type: 'movie',
+      media_id: movieId,
+      watchlist: true,
+    };
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set(
+        'Authorization',
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2YxY2U1ODUzNjdkMjIwNTE4YzIxYTA3NzA4OGIxMiIsInN1YiI6IjY1MTVlZWU5ZDQ2NTM3MDEwMDFiMWQ2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xiy3NtzMyacFesL7uRITqTDjyrlR-RAD7OIhpjGPqSk'
+      );
+
+    this._http.post(url, body, { headers }).subscribe(
+      (response) => {
+        console.log('Response:', response);
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  showwishList() :Observable<any>{
+    const url =
+      'https://api.themoviedb.org/3/account/20505794/watchlist/movies'; // Replace with the appropriate URL
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set(
+        'Authorization',
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2YxY2U1ODUzNjdkMjIwNTE4YzIxYTA3NzA4OGIxMiIsInN1YiI6IjY1MTVlZWU5ZDQ2NTM3MDEwMDFiMWQ2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xiy3NtzMyacFesL7uRITqTDjyrlR-RAD7OIhpjGPqSk'
+      );
+
+    const params = new HttpParams()
+      .set('language', 'en-US')
+      .set('page', '1')
+      .set('sort_by', 'created_at.asc');
+
+  return  this._http.get(url, { headers, params });
   }
 }
--- src/app/wishlist/wishlist-page/wishlist-page.component.ts
+++ src/app/wishlist/wishlist-page/wishlist-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MoviesApiService } from 'src/app/movies/services/movies-api.service';
 
 @Component({
   selector: 'app-wishlist-page',
   templateUrl: './wishlist-page.component.html',
-  styleUrls: ['./wishlist-page.component.css']
+  styleUrls: ['./wishlist-page.component.css'],
 })
 export class WishlistPageComponent {
-
+  constructor(private _movieAPI: MoviesApiService) {}
+myWishList =[]
+  ngOnInit(){
+    this._movieAPI.showwishList().subscribe(
+      (response) => {
+        console.log('Response:', response.results);
+        this.myWishList=response.results
+        // Handle the successful response here
+      },
+      (error) => {
+        console.error('Error:', error);
+        // Handle the error here
+      }
+    );
+  }
 }
